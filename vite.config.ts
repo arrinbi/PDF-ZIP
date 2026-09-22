37ee1a71c77e2174e15cbdd5e047e7717df8f117
arrinbi <arrinmlnar@gmail.com>
2026-09-22 21:26:11 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PDF-app/',
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
